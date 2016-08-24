88cf1b5e700d40060d26ff187f80a2d0012f4534
lazysong <hfutsong@gmail.com>
2016-08-24 11:37:22 +0800

    complete my fgets() and getline() and test them in main()

--- std_lib.c
+++ std_lib.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
-char *fgets(char *s, int n, FILE iop)
+char *myfgets(char *s, int n, FILE *iop)
 {
 	register char c;
 	register char *cs;
-	
+
 	cs = s;
-	while (--n > 0 && (c = fget(iop)) != EOF)
+	while (--n > 0 && (c = getc(iop)) != EOF)
 	{
 		if ((*cs++ = c) == '\n')
 			break;
@@ -15,7 +16,18 @@ char *fgets(char *s, int n, FILE iop)
 	return (c == EOF && cs == s) ? NULL : s;
 }
 
-char *getline(char *s, int n)
+int mygetline(char *line, int max)
 {
-	if (fgets(s, n, stdin))
+	if (myfgets(line, max, stdin) == NULL)
+		return 0;
+	else
+		return strlen(line);
+}
+
+int main()
+{
+	const int MAX = 20;
+	char line[MAX];
+	int n = mygetline(line, MAX);
+	return 0;
 }
